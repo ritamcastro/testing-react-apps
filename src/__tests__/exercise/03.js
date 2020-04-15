6fd54ecbe58163193728bd4fc1d6bf34aad41e2f
Rita Castro <rita@vw-dilab.com>
2020-04-15 16:24:26 +0100

    Ex: 3 - getByText and getByRole

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -1,20 +1,21 @@
-// Avoid implementation details
+ // Avoid implementation details
 // http://localhost:3000/counter
 import React from 'react'
 // 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+import {render, fireEvent, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
 
 test('counter increments when the button is clicked', () => {
-  const {container} = render(<Counter />)
+  const {container, getByRole, getByText} = render(<Counter />)
   // 🐨 replace these with screen queries
   // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+  const decrement = getByRole('button', {name: 'Decrement'})
+  const increment = getByRole('button', {name: 'Increment'})
+  // const message = container.firstChild.querySelector('div')
 
-  expect(message).toHaveTextContent('Current count: 0')
+  expect(getByText('Current count: 0')).toBeInTheDocument()
   fireEvent.click(increment)
-  expect(message).toHaveTextContent('Current count: 1')
+  expect(getByText('Current count: 1')).toBeInTheDocument()
   fireEvent.click(decrement)
-  expect(message).toHaveTextContent('Current count: 0')
+  expect(getByText('Current count: 0')).toBeInTheDocument()
 })
